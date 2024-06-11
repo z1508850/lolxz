aac0a79d606ba90062eeba11e14669330c2b1bb9
z1508850 <73526298+z1508850@users.noreply.github.com>
2024-06-11 17:47:10 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
 
   lang: 'zh-CN',
-  title: '茂茂物语',
+  title: 'LOL下载网盘',
   description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
   head,
 
